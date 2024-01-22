e9cde0168db23bcac583f4478739aad2722573a6
Your Name <you@example.com>
2024-01-22 17:41:32 +0100

    Marriage for one possible Pair

--- src/1-Containers/MainPlayContainer.jsx
+++ src/1-Containers/MainPlayContainer.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState, useImperativeHandle, forwardRef } from "react";
 import { cards } from "../Assets/Cards/cards";
 import randomIndex from "../Logic/randomIndex";
-
 import CpuCards from "../2-SubContainers/CpuCards";
 import MiddleCards from "../2-SubContainers/MiddleCards";
 import PlayerCards from "../2-SubContainers/PlayerCards";
@@ -14,6 +13,11 @@ const MainPlayContainer = forwardRef((props, ref) => {
   const [playedCardsOfTurn, setPlayedCardsOfTurn] = useState([]);
   const [trump, setTrump] = useState(null);
   const [isEnabled, setIsEnabled] = useState(true);
+  const [isExchangeEnabled, setIsExchangeEnabled] = useState(false);
+
+  const [isMarriageEnabled, setIsMarriageEnabled] = useState(false);
+  const [playerMarriage1, setPlayerMarriage1] = useState([]);
+  const [playerMarriage2, setPlayerMarriage2] = useState([]);
 
   useImperativeHandle(ref, () => ({
     handleNewGame,
@@ -66,8 +70,91 @@ const MainPlayContainer = forwardRef((props, ref) => {
         playedCardsOfTurn={playedCardsOfTurn}
         setPlayedCardsOfTurn={setPlayedCardsOfTurn}
       />
+      <button onClick={handleMarriage} className="in-game-btn in-game-btn-pair">
+        <img className="in-game-btn-img" src="/src/Assets/pair.svg" alt="" />
+      </button>
+      <button className="in-game-btn in-game-btn-exchange">
+        <img
+          className="in-game-btn-img"
+          src="/src/Assets/exchange.svg"
+          alt=""
+        />
+      </button>
     </div>
   );
+
+  function checkExchange() {
+    playerHand.map((c) => {
+      if (
+        c.color === trump.color &&
+        c.value === 2 &&
+        lastRoundWinner === "player"
+      ) {
+        setIsExchangeEnabled(true);
+      }
+    });
+  }
+
+  function checkMarriage() {
+    let matchingPairsArray1 = [];
+    let matchingPairsArray2 = [];
+    // check if there are marriage-qualified card among the hand
+    let marriageArray = playerHand.filter((c) => {
+      c.marriage === "m1" || "m2" || "m3" || "m4";
+    });
+    //if there is 1 or 2 pairs of same color, put them in an array
+    if (marriageArray.length >= 2) {
+      for (let i = 0; i < marriageArray.length; i++) {
+        for (let j = i + 1; j < marriageArray.length; j++) {
+          if (marriageArray[i].marriage === marriageArray[j].marriage) {
+            matchingPairsArray1 = [marriageArray[i], marriageArray[j]];
+          }
+        }
+      }
+    }
+    //if there are 2 pairs, put them in separate arrays
+    if (matchingPairsArray1 === 4) {
+      for (let i = 0; i < matchingPairsArray1.length; i++) {
+        for (let j = i + 1; j < matchingPairsArray1.length; j++) {
+          if (
+            matchingPairsArray1[i].marriage === matchingPairsArray1[j].marriage
+          ) {
+            matchingPairsArray2 = matchingPairsArray2.concat([
+              matchingPairsArray1[i],
+              matchingPairsArray1[j],
+            ]);
+            // sort in ascending order
+            matchingPairsArray2.sort((a, b) => {
+              a.value - b.value;
+            });
+            matchingPairsArray1.splice(j, 1);
+            matchingPairsArray1.splice(i, 1);
+          }
+        }
+      }
+    }
+    //enable the option
+    if (matchingPairsArray1.length > 0) {
+      matchingPairsArray1.sort((a, b) => {
+        a.value - b.value;
+      });
+      setPlayerMarriage1(matchingPairsArray1);
+      setPlayerMarriage2(matchingPairsArray2);
+      setIsMarriageEnabled(true);
+    }
+  }
+
+  function handleMarriage() {
+    if (playerMarriage2.length === 0) {
+      let newPlayerHand = playerHand.filter((c) => {
+        c.id !== playerMarriage1[0].id;
+      });
+      setPlayedCardsOfTurn(playerMarriage1[0]);
+      setPlayerHand(newPlayerHand);
+      setIsEnabled(false);
+    }
+    // this currently handles only the case, in which there is only on marriage-pair in the hand
+  }
 
   function handleNewGame() {
     let newRemainingCards = [...remainingCards];
@@ -124,6 +211,6 @@ const MainPlayContainer = forwardRef((props, ref) => {
   //   }
   // }
 });
-MainPlayContainer.displayName = "MainPlayContainer";
 
+MainPlayContainer.displayName = "MainPlayContainer";
 export default MainPlayContainer;
